9cc4f542360cf3834c497b35f63a7106ace14b58
Monirul Hasan <kmhasan@gmail.com>
2018-05-30 11:01:04 +0600

    profiling insertion sort

--- section3/Insertion Sort/main.cpp
+++ section3/Insertion Sort/main.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <cstdlib>
+#include <ctime>
 using namespace std;
 
 void printArray(int A[], int length) {
@@ -11,7 +13,10 @@ void printArray(int A[], int length) {
 }
 
 void generateArray(int A[], int length) {
-// WRITE YOUR CODE HERE
+    srand(time(NULL));
+    for (int i = 0; i < length; i++) {
+        A[i] = random() % 100;
+    }
 }
 
 void insertionSort(int A[], int length) {
@@ -27,23 +32,35 @@ void insertionSort(int A[], int length) {
 }
 
 int main() {
-    // Homework
-    // set n = 10000
-    // generate n random numbers
-    // to fill out the array
-    // Hint: use srand and random
-    // functions from cstdlib header
-    // generateArray(array, 10000);
-    int array[] = {56, 88, 32, 21, 14, 100, 56, 32, 19};
-    int n = 9;
+/*
+H/W #1: Try this program with different values of
+n. n = 1, 10, 100, ... , up to 10 million
+Run the program multiple times for each n value
+Write them down on a table (on spreadsheet) or on paper
 
-    cout << "Before sorting: ";
-    printArray(array, n);
+H.W #2: Implement bubble sort and run the same
+experiments and tabulate your findings
+*/
+    int n = 200000;
+//    int array[n]; // static allocation
+    int *array = new int[n]; // dynamic allocation
+    generateArray(array, n);
+
+//    cout << "Before sorting: ";
+//    printArray(array, n);
 
     // Insertion Sort
+    clock_t start = clock();
     insertionSort(array, n);
+    clock_t stop = clock();
 
-    cout << "After sorting:  ";
-    printArray(array, n);
+    long clockTicks = stop - start;
+    cout << "n = " << n << endl;
+    cout << "clock ticks = " << clockTicks << endl;
+    cout << "clocks per sec = " << CLOCKS_PER_SEC << endl;
+    double timeTaken = (double) clockTicks / CLOCKS_PER_SEC;
+    cout << "time taken " << timeTaken << " seconds " << endl;
+    //cout << "After sorting:  ";
+    //printArray(array, n);
 
 }
