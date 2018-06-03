b57ac365435f46a89f9cd001b715997f6d9e3be3
Monirul Hasan <kmhasan@gmail.com>
2018-06-03 11:01:54 +0600

    lab 2

--- section3/Insertion Sort/main.cpp
+++ section3/Insertion Sort/main.cpp
@@ -19,6 +19,20 @@ void generateArray(int A[], int length) {
     }
 }
 
+void selectionSort(int A[], int length) {
+    for (int j = 0; j < length - 1; j++) {
+        int minIndex = j;
+        for (int i = j + 1; i < length; i++)
+            if (A[i] < A[minIndex])
+                minIndex = i;
+        if (minIndex != j) {
+            int temp = A[j];
+            A[j] = A[minIndex];
+            A[minIndex] = temp;
+        }
+    }
+}
+
 void insertionSort(int A[], int length) {
     for (int j = 1; j < length; j++) {
         int key = A[j];
@@ -41,26 +55,53 @@ Write them down on a table (on spreadsheet) or on paper
 H.W #2: Implement bubble sort and run the same
 experiments and tabulate your findings
 */
-    int n = 200000;
+    int n = 100000;
 //    int array[n]; // static allocation
-    int *array = new int[n]; // dynamic allocation
-    generateArray(array, n);
+    int *array1 = new int[n]; // dynamic allocation
+    generateArray(array1, n);
+
+    int *array2 = new int[n];
+    for (int i = 0; i < n; i++)
+        array2[i] = array1[i];
 
 //    cout << "Before sorting: ";
 //    printArray(array, n);
 
-    // Insertion Sort
+    // Selection Sort
     clock_t start = clock();
-    insertionSort(array, n);
+    selectionSort(array1, n);
     clock_t stop = clock();
 
+//    cout << "After sorting:  ";
+//    printArray(array, n);
+
     long clockTicks = stop - start;
+    cout << "Algorithm: Selection Sort" << endl;
     cout << "n = " << n << endl;
     cout << "clock ticks = " << clockTicks << endl;
     cout << "clocks per sec = " << CLOCKS_PER_SEC << endl;
     double timeTaken = (double) clockTicks / CLOCKS_PER_SEC;
     cout << "time taken " << timeTaken << " seconds " << endl;
-    //cout << "After sorting:  ";
-    //printArray(array, n);
+
+//    cout << "Before sorting: ";
+//    printArray(array, n);
+
+    // Insertion Sort
+    start = clock();
+    insertionSort(array1, n);
+    stop = clock();
+
+//    cout << "After sorting:  ";
+//    printArray(array, n);
+
+    cout << endl;
+
+    clockTicks = stop - start;
+    cout << "Algorithm: Insertion Sort" << endl;
+    cout << "n = " << n << endl;
+    cout << "clock ticks = " << clockTicks << endl;
+    cout << "clocks per sec = " << CLOCKS_PER_SEC << endl;
+    timeTaken = (double) clockTicks / CLOCKS_PER_SEC;
+    cout << "time taken " << timeTaken << " seconds " << endl;
 
 }
